7e40dde4db07867ae8e7df64da30c55b2de3b44d
Tatiana Zhukova <tatiana.s.zhukova@yandex.ru>
2023-02-21 21:12:31 +0500

    add new subview and frame

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    let profileHeaderView = ProfileHeaderView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        view.backgroundColor = .lightGray
 
-    /*
-    // MARK: - Navigation
+        title = "Profile"
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        profileHeaderView.backgroundColor = .white
+        profileHeaderView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: 1000,
+            height: 90
+        )
+
+        view.addSubview(profileHeaderView)
+    }
 
 }
