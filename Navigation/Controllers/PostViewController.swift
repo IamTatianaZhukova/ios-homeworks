b85ffbb27ae5884940afd3f6ace30982661e1b53
Tatiana Zhukova <tatiana.s.zhukova@yandex.ru>
2023-02-27 19:48:51 +0500

    Delete Buttom from PostViewContoller

--- Navigation/Controllers/PostViewController.swift
+++ Navigation/Controllers/PostViewController.swift
@@ -10,14 +10,7 @@ import UIKit
 class PostViewController: UIViewController {
 
     var post: Post?
-    
-    private lazy var actionButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Прочитать пост", for: .normal)
 
-        return button
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +18,8 @@ class PostViewController: UIViewController {
 
         view.backgroundColor = .systemIndigo
 
-        view.addSubview(actionButton)
 
-        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
-        NSLayoutConstraint.activate([
-            actionButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20.0),
-            actionButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20.0),
-            actionButton.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
-            actionButton.heightAnchor.constraint(equalToConstant: 44.0)
-        ])
-        
-        actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
     }
-    @objc func buttonPressed(_ sender: UIButton) {
-        let infoViewController = InfoViewController()
-
-        InfoViewController().modalTransitionStyle = .coverVertical
-        InfoViewController().modalPresentationStyle = .pageSheet
-
-        present(infoViewController, animated: true)
-    }
-    
 
     /*
     // MARK: - Navigation
