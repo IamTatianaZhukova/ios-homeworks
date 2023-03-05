4a5485a6f71778d8ef54fadc7f4dd3163337ef25
Tatiana Zhukova <tatiana.s.zhukova@yandex.ru>
2023-03-05 19:59:19 +0500

    chage all

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -1,15 +1,94 @@
-//
-//  ProfileHeaderView.swift
-//  Navigation
-//
-//  Created by Татьяна Жукова on 19.02.2023.
-//
-
 import UIKit
 
 class ProfileHeaderView: UIView {
 
+    private let profilePhoto: UIImageView = {
+        let thePhoto = UIImageView()
+        thePhoto.translatesAutoresizingMaskIntoConstraints = false
+        thePhoto.image = UIImage(named: "ProfilePhoto")
+        thePhoto.layer.borderWidth = 3.0
+        thePhoto.layer.borderColor = UIColor.white.cgColor
+        thePhoto.layer.cornerRadius = 75
 
- 
-    
+        return thePhoto
+        }()
+
+    private let nameLabel: UILabel = {
+        let theName = UILabel()
+        theName.translatesAutoresizingMaskIntoConstraints = false
+        theName.text = "Alina Alinovna"
+        theName.textColor = UIColor.black
+        theName.adjustsFontSizeToFitWidth = true
+        theName.font = UIFont(name: "HelveticaNeue-Bold", size: 19.0)
+
+        return theName
+    }()
+
+    private lazy var showStatusButton: UIButton = {
+        let statusButton = UIButton()
+        statusButton.translatesAutoresizingMaskIntoConstraints = false
+        statusButton.setTitle("Show status", for: .normal)
+        statusButton.setTitleColor(.white, for: .normal)
+        statusButton.backgroundColor = UIColor.blue
+        statusButton.layer.cornerRadius = 4.0
+        statusButton.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
+        statusButton.layer.shadowRadius = 4.0
+        statusButton.layer.shadowColor = UIColor.black.cgColor
+        statusButton.layer.shadowOpacity = 0.7
+
+        return statusButton
+    }()
+
+    private let statusLabel: UILabel = {
+        let theStatus = UILabel()
+        theStatus.translatesAutoresizingMaskIntoConstraints = false
+        theStatus.text = "Waiting for something..."
+        theStatus.textColor = UIColor.systemGray
+        theStatus.adjustsFontSizeToFitWidth = true
+        theStatus.font = UIFont(name: "HelveticaNeue-Regular", size: 14.0)
+
+        return theStatus
+    }()
+
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupConstraints() {
+        addSubview(profilePhoto)
+        addSubview(nameLabel)
+        addSubview(showStatusButton)
+        addSubview(statusLabel)
+
+        NSLayoutConstraint.activate([
+            profilePhoto.widthAnchor.constraint(equalToConstant: 150),
+            profilePhoto.heightAnchor.constraint(equalToConstant: 150),
+            profilePhoto.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
+            profilePhoto.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16.0),
+
+            nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 180.0),
+            nameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27.0),
+
+            showStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
+            showStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
+//            showStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            showStatusButton.bottomAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 66.0),
+
+            statusLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 180.0),
+            statusLabel.topAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -59.0)
+        ])
+
+        showStatusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        showStatusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+    }
+
+    @objc func buttonPressed(_ sender: UIButton) {
+        guard let statusText = statusLabel.text else { return }
+                print("Status text: \(statusText)")
+    }
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,117 +10,32 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     let profileHeaderView = ProfileHeaderView()
-
-    let profilePhoto: UIImageView = {
-        let thePhoto = UIImageView()
-        thePhoto.translatesAutoresizingMaskIntoConstraints = false
-        thePhoto.image = UIImage(named: "ProfilePhoto")
-        thePhoto.layer.borderWidth = 3.0
-        thePhoto.layer.borderColor = UIColor.white.cgColor
-        thePhoto.layer.cornerRadius = 75
-
-        return thePhoto
-        }()
-
-    let nameLabel: UILabel = {
-        let theName = UILabel()
-        theName.translatesAutoresizingMaskIntoConstraints = false
-        theName.text = "Alina Alinovna"
-        theName.textColor = UIColor.black
-        theName.adjustsFontSizeToFitWidth = true
-        theName.font = UIFont(name: "HelveticaNeue-Bold", size: 19.0)
-
-        return theName
-    }()
-
-    private lazy var showStatusButton: UIButton = {
-        let statusButton = UIButton()
-        statusButton.translatesAutoresizingMaskIntoConstraints = false
-        statusButton.setTitle("Show status", for: .normal)
-        statusButton.setTitleColor(.white, for: .normal)
-        statusButton.backgroundColor = UIColor.blue
-        statusButton.frame = CGRect(
-            x: 100.0,
-            y: 200.0,
-            width: 200.0,
-            height: 50.0
-        )
-        
-        return statusButton
-    }()
-
-    let statusLabel: UILabel = {
-        let theStatus = UILabel()
-        theStatus.translatesAutoresizingMaskIntoConstraints = false
-        theStatus.text = "Waiting for something..."
-        theStatus.textColor = UIColor.systemGray
-        theStatus.adjustsFontSizeToFitWidth = true
-        theStatus.font = UIFont(name: "HelveticaNeue-Regular", size: 14.0)
-
-        return theStatus
-    }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.addSubview(profileHeaderView)
         view.backgroundColor = .lightGray
 
         title = "Profile"
 
-        view.addSubview(profilePhoto)
-        view.addSubview(nameLabel)
-        view.addSubview(showStatusButton)
-
-        showStatusButton.layer.cornerRadius = 4.0
-        showStatusButton.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
-        showStatusButton.layer.shadowRadius = 4.0
-        showStatusButton.layer.shadowColor = UIColor.black.cgColor
-        showStatusButton.layer.shadowOpacity = 0.7
-        showStatusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
-
-        view.addSubview(statusLabel)
-
-        setupConstraints()
-    }
-
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            profilePhoto.widthAnchor.constraint(equalToConstant: 150),
-            profilePhoto.heightAnchor.constraint(equalToConstant: 150),
-            profilePhoto.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
-            profilePhoto.topAnchor.constraint(equalTo: view.topAnchor, constant: 106.0),
-
-            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 180.0),
-            nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 117.0),
-
-            showStatusButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
-            showStatusButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
-            showStatusButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            showStatusButton.bottomAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 41.0),
-
-            statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 180.0),
-            statusLabel.topAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -59.0)
-        ])
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
 
         profileHeaderView.backgroundColor = .white
-        profileHeaderView.frame = CGRect(
+        let profileHeaderViewHeight: CGFloat = 90.0
+        let profileHeaderViewFrame = CGRect(
             x: 0,
             y: 0,
-            width: 1000,
-            height: 90
+            width: self.view.bounds.width,
+            height: profileHeaderViewHeight
         )
 
-        view.addSubview(profileHeaderView)
+        profileHeaderView.frame = profileHeaderViewFrame
 
     }
 
-    @objc func buttonPressed(_ sender: UIButton) {
-        guard let statusText = statusLabel.text else { return }
-                print("Status text: \(statusText)")
-    }
 
 }
