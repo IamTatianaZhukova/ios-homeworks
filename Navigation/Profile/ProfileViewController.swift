c6bffc5fa2369b276e98ac9a3e8d8b3d15ad48f0
Tatiana Zhukova <tatiana.s.zhukova@yandex.ru>
2023-03-13 21:52:46 +0500

    Change newButton again

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -31,8 +31,6 @@ class ProfileViewController: UIViewController {
 
         setupConstraints()
 
-        newButton.addTarget(self, action: #selector(newButtonPressed), for: .touchUpInside)
-
     }
 
     private func setupConstraints() {
